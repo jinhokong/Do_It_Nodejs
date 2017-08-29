31f5cab03e83cd5a40ad9c66492c3e32cd09ec90
kong22 <tpdleps@gmail.com>
2017-08-29 15:25:54 +0900

    ch02-01

--- example1/test.js
+++ example1/test.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+console.log('this is number. %d',10);
+console.log('this is string. %s','hi');
+var person ={
+    name:'hong',
+    age:20
+};
+console.log('this is javascript object. %j',person);
+console.
