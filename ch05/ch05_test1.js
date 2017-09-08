e4529a6baa11ff21be9fb3a0a3dac97bf7fa5762
kong22 <tpdleps@gmail.com>
2017-09-08 18:22:15 +0900

    ch05_02

--- ch05/ch05_test1.js
+++ ch05/ch05_test1.js
@@ -2,6 +2,7 @@ var http=require('http');
 
 var server=http.createServer();
 var port=3000;
-server.listen(port,function(){
-    console.log('웹서버가 실행되었습니다.'+port);
+var host='192.168.0.20';
+server.listen(port,host,'50000',function(){
+    console.log('웹서버가 실행되었습니다.'+host+':'+port);
 })
