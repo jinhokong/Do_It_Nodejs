5a59534741f1662675e8d5c498543c101fde23e1
kong22 <tpdleps@gmail.com>
2017-09-21 18:58:47 +0900

    ch05_11

---
+++ ch05/app11.js
@@ -0,0 +1,57 @@
+var express=require('express');
+var http=require('http');
+var statics=require('serve-static');
+var path=require('path');
+var cookiePaser=require('cookie-parser');
+var bodyparser=require('body-parser');
+var app=express();
+
+
+app.set('port',process.env.PORT||3000);
+
+app.use(statics(path.join(__dirname,'public')));
+
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(bodyparser.json());
+
+app.use(cookiePaser())
+
+var router=express.Router();
+
+router.route('/process/setUserCookie').get(function(req,res){
+    console.log('/porcess/setUserCookie 라우팅 함수 호출');
+
+    res.cookie('user',{
+        id:'mike',
+        name:'소녀시대',
+        authorized:true
+    });
+    res.redirect('/process/showCookie');
+});
+
+router.route('/process/showCookie').get(function(req,res){
+    console.log('/process/showCookie 라우팅 함수 호출됨');
+    res.send(req.cookies);
+});
+
+router.route('/process/login/:name').post(function(req,res){
+    console.log('/process/login/:name 라우팅 함수에서 받음.');
+    var paramName=req.params.name;
+    var paramId=req.body.id||req.query.id;
+    var paramPassword=req.body.password||req.query.password;
+    res.writeHead(200,{"Content-Type":"text/html;charset=utf8"});
+    res.write("<h1>서버에서 로그인 응답</h1>");
+    res.write("<div><p>"+paramName+"</p></div>");    
+    res.write("<div><p>"+paramId+"</p></div>");
+    res.write("<div><p>"+paramPassword+"</p></div>");
+    res.end();
+    
+})
+
+app.use('/',router);
+app.all('*',function(req,res){
+    res.status(404).send('요청한 페이지가 없습니다.')
+})
+var server=http.createServer(app).listen(app.get('port'),function(){
+    console.log('익스프레스로 웹서버를 실행함:'+app.get('port'));
+});
