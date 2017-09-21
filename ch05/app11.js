024dc59e8b0493ee41bde9db0403a48e6fb2ffd4
kong22 <tpdleps@gmail.com>
2017-09-21 20:30:40 +0900

    ch05_13

--- ch05/app11.js
+++ ch05/app11.js
@@ -10,7 +10,7 @@ var app=express();
 
 app.set('port',process.env.PORT||3000);
 
-app.use(statics(path.join(__dirname,'public')));
+app.use('/public',statics(path.join(__dirname,'public')));
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
@@ -29,7 +29,53 @@ router.route('/process/product').get(function(req,res){
     if(req.session.user){
         res.redirect('/public/product.html');
     }else{
-        res.redirect('public/login2.html');
+        res.redirect('/public/login2.html');
+    }
+})
+
+
+router.route('/process/login').post(function(req,res){
+    console.log('process/login 라우팅 함수 호출');
+
+    var paramID=req.body.id||req.query.id;
+    var paramPassword=req.body.password||req.query.password;
+
+    console.log('요청 파라미터'+paramID+","+paramPassword);
+
+    if(req.session.user){
+        console.log('이미 로그인 되어 있습니다.');
+        res.redirect('/public/product.html');
+    }else{
+        req.session.user={
+            id:paramID,
+            name:'소녀시대',
+            authorized:true
+        };
+
+        res.writeHead(200,{"Content-Type":"text/html;charset=utf8"})
+        res.write('<h1>로그인 성공</h1>');
+        res.write('<p>ID:'+paramID+'</p>');
+        res.write('<br><br><a href="/public/product.html">상품페이지로 이동</a>')
+        res.end();
+    }
+})
+
+router.route('/process/logout').get(function(req,res){
+    console.log('/process/logout 라우팅 함수 호출');
+    if(req.session.user){
+        console.log('로그아웃합니다');
+        req.session.destroy(function(err){
+            if(err){
+                console.log('세션 삭제 시 에러 발생.');
+                return;
+            }
+            console.log('세션 삭제 성공');
+            res.redirect('/public/login2.html');
+
+        });
+    }else{
+        console.log('로그인 되어 있지 않습니다.');
+        res.redirect('/public/login2.html');
     }
 })
 
