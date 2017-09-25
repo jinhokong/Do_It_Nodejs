a857302b982c69b013fa9381f2114bb250af2af2
jinhoKong <tpdleps@gmail.com>
2017-09-25 19:41:34 +0900

    ch07_02_2
    ch07_02_2

--- ch07/module_test6.js
+++ ch07/module_test6.js
@@ -13,3 +13,8 @@ function showUser(){
     return user.getUser().name+', '+user.group.name;
 }
 console.log('사용자 정보:'+showUser());
+
+var a = [1,2,3,4,5];
+var b = a.forEach( function(v, i){ console.log(v); return v+1;});
+var c = a.map( function(v, i){ console.log(v); return v+1;});
+console.log(typeof(b));
