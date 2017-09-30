6f3e69e386c227d8d57f74ff9d5c4ce4ec23ca14
jinhoKong <tpdleps@gmail.com>
2017-10-01 01:41:51 +0900

    ch09_05

--- ch09/app.js
+++ ch09/app.js
@@ -120,10 +120,44 @@ passport.use('local-signup',new localStrategy({
 	passReqToCallback:true
 },function(req,email,password,done){
 	var paramName=req.body.name||req.query.name;
-	console.log('passport의 local-signup 호출됨:'+email+','+password)
-}
-))
+	console.log('passport의 local-signup 호출됨:'+email+','+password);
+	var database=app.get('database');
+	database.UserModel.findOne({'email':email},function(err,user){
+		if (err){
+			console.log('error!!!!!')
+			return done(err);
+		}
+		if(user){
+			console.log('기존에 계정이 있습니다');
+			return done(null,false,req.flash('signupMessage','계정이있습니다'))
 
+		}
+		else{
+			var user= new database.UserModel({'email':email,'password':password,'name':paramName});
+			user.save(function(err){
+				if(err){
+					console.log('데이터베이스에 저장시 에러.')
+					return done(null,false,req.flash('signupMessage','사용자 정보 저장시 에러가 발생했습니다.'))
+				}
+				console.log('사용자 데이터 저장함');
+				return done(null,user);
+			})
+		}
+	})
+}
+));
+
+
+passport.serializeUser(function(user,done){
+	console.log('serializeUser 호출됨,');
+	console.dir(user);
+	done(null,user);
+})
+passport.deserializeUser(function(user,done){
+	console.log('deserializeUser호출됨');
+	console.dir(user);
+	done(null,user);
+})
 //라우팅 정보를 읽어들여 라우팅 설정
 route_loader.init(app, express.Router());
 
